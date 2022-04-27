2f1a1d792901acef358f6ee4f09cd39daf6a30de
Hugo <Hugo>
2022-04-27 09:21:34 +0200

    plans crud page

--- components/plan/form.vue
+++ components/plan/form.vue
@@ -1,16 +1,6 @@
 <template>
-  <v-navigation-drawer
-    temporary
-    right
-    fixed
-    v-model="drawer"
-    width="40%"
-    hide-overlay
-    stateless
-  >
-    <p
-      class="form-title pa-2 title font-weight-regular text-uppercase d-flex justify-space-between"
-    >
+  <v-navigation-drawer temporary right fixed v-model="drawer" width="40%" hide-overlay stateless>
+    <p class="form-title pa-2 title font-weight-regular text-uppercase d-flex justify-space-between">
       {{ !payload.id ? "Add new" : "Edit" }} Plan
       <v-btn icon small @click="drawer = false">
         <v-icon>mdi-close</v-icon>
@@ -27,47 +17,23 @@
               </p>
             </v-flex>
             <v-flex xs12>
-              <ValidationProvider
-                slim
-                name="name"
-                rules="required|min:1|max:100"
-                v-slot="{ errors }"
-              >
+              <ValidationProvider slim name="name" rules="required|min:1|max:100" v-slot="{ errors }">
                 <div class="mb-1">
                   <p class="subtitle-2 font-weight-regular mb-2">
                     <span>*</span>
                     Name
                   </p>
-                  <v-text-field
-                    v-model="payload.name"
-                    placeholder="Type name..."
-                    type="text"
-                    hide-details="auto"
-                    solo
-                    :error-messages="errors"
-                  ></v-text-field>
+                  <v-text-field v-model="payload.name" placeholder="Type name..." type="text" hide-details="auto" solo :error-messages="errors"></v-text-field>
                 </div>
               </ValidationProvider>
             </v-flex>
             <v-flex xs12>
-              <ValidationProvider
-                slim
-                name="description"
-                rules="max:200"
-                v-slot="{ errors }"
-              >
+              <ValidationProvider slim name="description" rules="max:200" v-slot="{ errors }">
                 <div class="mb-1">
                   <p class="subtitle-2 font-weight-regular mb-2">
                     Description
                   </p>
-                  <v-textarea
-                    v-model="payload.description"
-                    placeholder="Type description..."
-                    type="text"
-                    hide-details="auto"
-                    solo
-                    rows="2"
-                  ></v-textarea>
+                  <v-textarea v-model="payload.description" placeholder="Type description..." type="text" hide-details="auto" solo rows="2"></v-textarea>
                 </div>
               </ValidationProvider>
             </v-flex>
@@ -78,15 +44,12 @@
             </v-flex>
             <v-flex xs12>
               <v-expansion-panels v-model="panel">
-                <v-expansion-panel
-                  v-for="(price, i) in payload.prices"
-                  :key="i"
-                >
+                <v-expansion-panel v-for="(price, i) in payload.prices" :key="i">
                   <v-expansion-panel-header>
                     <div class="d-flex justify-space-between">
                       {{ getPriceTitle(price) }}
                       <v-btn text color="error" small @click.prevent.stop="removePrice(i)" v-if="payload.prices.length != 1">
-                        REmove
+                        Remove
                       </v-btn>
                     </div>
                   </v-expansion-panel-header>
@@ -96,28 +59,13 @@
                         <span>*</span>
                         Price
                       </p>
-                      <v-text-field
-                        placeholder="Type price..."
-                        type="text"
-                        hide-details="auto"
-                        solo
-                        prefix="€"
-                        v-model="price.price"
-                      ></v-text-field>
+                      <v-text-field placeholder="Type price..." type="number" hide-details="auto" solo prefix="€" v-model="price.price" @change="price.price = parseFloat(price.price)"></v-text-field>
                     </div>
                     <div class="d-flex mb-2 justify-space-between">
-                      <v-btn
-                        :class="[price.is_recurring ? 'primary' : 'normal']"
-                        style="width: 49%"
-                        @click="price.is_recurring = 1"
-                      >
+                      <v-btn :class="[price.is_recurring ? 'primary' : 'normal']" style="width: 49%" @click="price.is_recurring = 1">
                         RECURRING
                       </v-btn>
-                      <v-btn
-                        :class="[!price.is_recurring ? 'primary' : 'normal']"
-                        style="width: 49%"
-                        @click="price.is_recurring = 0"
-                      >
+                      <v-btn :class="[!price.is_recurring ? 'primary' : 'normal']" style="width: 49%" @click="price.is_recurring = 0">
                         ONE TIME ONLY
                       </v-btn>
                     </div>
@@ -126,14 +74,7 @@
                         <span>*</span>
                         Billing Period
                       </p>
-                      <v-select
-                        v-model="price.billing_period"
-                        :items="periodOptions"
-                        hide-details="auto"
-                        solo
-                        item-text="text"
-                        item-value="id"
-                      ></v-select>
+                      <v-select v-model="price.billing_period" :items="periodOptions" hide-details="auto" solo item-text="text" item-value="id"></v-select>
                     </div>
                     <template v-if="price.billing_period == 7">
                       <div class="mb-2">
@@ -141,40 +82,26 @@
                           <span>*</span>
                           Cycle Count
                         </p>
-                        <v-text-field
-                          placeholder="Type total cycle..."
-                          type="text"
-                          hide-details="auto"
-                          solo
-                          v-model="price.recycle_count"
-                        ></v-text-field>
+                        <v-text-field placeholder="Type total cycle..." type="text" hide-details="auto" solo v-model="price.recycle_count"></v-text-field>
                       </div>
                       <div class="mb-2">
                         <p class="subtitle-2 font-weight-regular mb-2">
                           <span>*</span>
                           Cycle Type
                         </p>
-                        <v-select
-                          v-model="price.recycle_type"
-                          :items="recycleOptions"
-                          hide-details="auto"
-                          solo
-                          item-text="text"
-                          item-value="id"
-                        ></v-select>
+                        <v-select v-model="price.recycle_type" :items="recycleOptions" hide-details="auto" solo item-text="text" item-value="id"></v-select>
                       </div>
                     </template>
-                    <div>
-                      <v-btn class="primary" block @click="addNewPlanPrice">
-                        ADD NEW PRICE
-                        <v-icon>mdi-plus</v-icon>
-                      </v-btn>
-                    </div>
+
                   </v-expansion-panel-content>
                 </v-expansion-panel>
               </v-expansion-panels>
             </v-flex>
             <v-flex xs12>
+              <v-btn class="primary" block @click="addNewPlanPrice">
+                ADD NEW PRICE
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
               <v-btn class="success mt-1" block type="submit">
                 <v-icon>mdi-content-save-outline</v-icon>
                 {{ payload.id ? "UPDATE" : "SAVE" }}
@@ -200,7 +127,7 @@ export default {
             price: 0,
             is_recurring: 1,
             billing_period: 3,
-            recycle_count: 2,
+            recycle_count: 1,
             recycle_type: 3,
           },
         ],
@@ -219,11 +146,11 @@ export default {
         { id: 6, text: "Every Year" },
         { id: 7, text: "Custom" },
       ],
-      recycleOptions:[
+      recycleOptions: [
         { id: 1, text: "Daily" },
         { id: 2, text: "Weekly" },
         { id: 3, text: "Monthly" },
-      ]
+      ],
     };
   },
   props: {
@@ -242,7 +169,7 @@ export default {
         price: 0,
         is_recurring: 1,
         billing_period: 3,
-        recycle_count: 2,
+        recycle_count: 1,
         recycle_type: 3,
       });
     },
@@ -257,12 +184,28 @@ export default {
       });
     },
     getPriceTitle(price) {
-      return `€ ${price.price ? price.price : "0.00"} EURO / Month`;
+      if (!price.price) price.price = 0;
+      let newPrice = `${parseFloat(price.price).toFixed(2)} €`;
+
+      const suffixes = ["day", "week", "month", "3 months", "6 months", "year"];
+      const types = ["day", "week", "month"];
+      let suffix = "";
+
+      if (price.is_recurring) suffix += "every ";
+
+      if (price.billing_period == 7)
+        suffix += `${price.recycle_count} ${types[price.recycle_type - 1]}${
+          price.recycle_count > 1 ? "s" : ""
+        }`;
+      else if (price.billing_period)
+        suffix += suffixes[price.billing_period - 1];
+
+      return `${newPrice} / ${suffix}`;
     },
     removePrice(index) {
-      if(this.payload.prices.length == 1) return
-      this.payload.prices.splice(index, 1)
-    }
+      if (this.payload.prices.length == 1) return;
+      this.payload.prices.splice(index, 1);
+    },
   },
   watch: {
     drawerStatus(val) {
@@ -284,9 +227,9 @@ export default {
             price: 0,
             is_recurring: 1,
             billing_period: 3,
-            recycle_count: 2,
+            recycle_count: 1,
             recycle_type: 3,
-          })
+          });
         }
         if (!this.originalPayload) {
           this.originalPayload = this.cloneVariable(this.payload);
--- pages/settings/plans/index.vue
+++ pages/settings/plans/index.vue
@@ -1,47 +1,22 @@
 <template>
   <v-container>
-    <form-drawer :drawerStatus="drawer" @closeDrawer="drawer = !drawer"
-      @addRecord="addRecord($event)"
-      @updateRecord="updateRecord($event)"
-      :selectedItem="selectedItem"
-    >
+    <form-drawer :drawerStatus="drawer" @closeDrawer="drawer = !drawer" @addRecord="addRecord($event)" @updateRecord="updateRecord($event)" :selectedItem="selectedItem">
     </form-drawer>
-    <data-table
-      :options="options"
-      :title="title"
-      :headers="headers"
-      :data="data"
-      searchPlaceholder="Short name, Long name, Code"
-      class="custom-table"
-      @addRecord="drawer = !drawer"
-      @deleteRecord="deleteRecord($event)"
-      @reloadtable="initialize()"
-      @FilterBy="filterBy($event)"
-      @updatePagenum="updatePagenum($event)"
-      @searchRecords="searchRecords($event)"
-      @editRecord="editRecord($event)"
-    >
+    <data-table :options="options" :title="title" :headers="headers" :data="data" searchPlaceholder="Short name, Long name, Code" class="custom-table" @addRecord="drawer = !drawer" @deleteRecord="deleteRecord($event)" @reloadtable="initialize()" @FilterBy="filterBy($event)" @updatePagenum="updatePagenum($event)" @searchRecords="searchRecords($event)" @editRecord="editRecord($event)">
       <template v-slot:status="{item}">
-        <v-switch
-          v-model="item.status"
-          inset
-          color="success"
-          dense
-          hide-details=""
-          @change="updateDefaultValue(item)"
-        ></v-switch>
+        <v-switch v-model="item.status" inset color="success" dense hide-details="" @change="updateDefaultValue(item)"></v-switch>
       </template>
-       <template v-slot:prices="{item}">
-         <template v-if="item.prices.length">
-          <template v-for="(price, index) of item.prices" >
+      <template v-slot:prices="{item}">
+        <template v-if="item.prices.length">
+          <template v-for="(price, index) of item.prices">
             <v-chip small outlined label class="mr-1 mb-1" color="primary" :key="`${index}-price`">
-              {{priceTitle(price)}}
+              {{ price | displayPrice }}
             </v-chip>
           </template>
-         </template>
-         <template v-else>
+        </template>
+        <template v-else>
           ...
-         </template>
+        </template>
         <!-- {{formatDate(item.created_at)}} -->
       </template>
       <template v-slot:created_at="{item}">
@@ -59,87 +34,114 @@ import tableHelper from "~/mixins/tableHelper.vue";
 import dateHelper from "~/mixins/dateHelper.vue";
 import formDrawer from "~/components/plan/form.vue";
 export default {
-  components: { dataTable, formDrawer},
-  mixins:[tableHelper, dateHelper],
+  components: { dataTable, formDrawer },
+  mixins: [tableHelper, dateHelper],
   data() {
     return {
       options: {},
       title: "Plans",
       headers: [
-        { text: "#", value: "id", width:'2%'},
-        { text: "Name", value: "name"},
+        { text: "#", value: "id", width: "2%" },
+        { text: "Name", value: "name" },
         { text: "Description", value: "description" },
         { text: "Price", value: "prices" },
         { text: "Active", value: "status" },
-        { text: "Created at", value: "created_at"},
-        { text: "Updated at", value: "updated_at"},
-        { text: "Action", value: "action"},
+        { text: "Created at", value: "created_at" },
+        { text: "Updated at", value: "updated_at" },
+        { text: "Action", value: "action" },
       ],
       data: [],
-      drawer:false,
-      selectedItem:{}
+      drawer: false,
+      selectedItem: {},
     };
   },
   mounted() {
-    this.initialize()
+    this.initialize();
   },
   methods: {
     initialize() {
-      this.$axios.get(`${this.$plans}?${this.urlQuery()}&relations=prices`).then(({data}) => {
-        this.data = data.data
-        this.options = data.options
-      })
+      this.$axios
+        .get(`${this.$plans}?${this.urlQuery()}&relations=prices`)
+        .then(({ data }) => {
+          this.data = data.data;
+          this.options = data.options;
+        });
     },
     addRecord(payload) {
       this.create().then(() => {
-        this.$axios.post(`${this.$plans}`, payload).then(({data}) => {
-          this.successNotification(data, 'added', 'country', 'plans', 'name')
-          this.initialize()
-        })
-      })
+        this.$axios.post(`${this.$plans}`, payload).then(({ data }) => {
+          this.successNotification(data, "added", "country", "plans", "name");
+          this.initialize();
+        });
+      });
     },
     deleteRecord(items) {
       this.delete().then(() => {
-        let ids = this.getIds(items)
-        this.$axios.delete(`${this.$plans}/${ids}`).then(({data}) => {
-          this.successNotification(items, 'deleted', 'country', 'plans', 'name')
-          this.initialize()
-        })
-      })
+        let ids = this.getIds(items);
+        this.$axios.delete(`${this.$plans}/${ids}`).then(({ data }) => {
+          this.successNotification(
+            items,
+            "deleted",
+            "country",
+            "plans",
+            "name"
+          );
+          this.initialize();
+        });
+      });
     },
     updateDefaultValue(item) {
-      let payload = {is_default:item.is_default}
-      this.$axios.put(`${this.$plans}/${item.id}/default`, payload).then(({data}) => {
-        this.successNotification(item, 'set as default', 'country', 'plans', 'name')
-      })
+      let payload = { is_default: item.is_default };
+      this.$axios
+        .put(`${this.$plans}/${item.id}/default`, payload)
+        .then(({ data }) => {
+          this.successNotification(
+            item,
+            "set as default",
+            "country",
+            "plans",
+            "name"
+          );
+        });
     },
     editRecord(item) {
-      this.drawer = !this.drawer
-      this.selectedItem = this.cloneVariable(item)
+      this.drawer = !this.drawer;
+      this.selectedItem = this.cloneVariable(item);
     },
     updateRecord(payload) {
       this.update().then(() => {
-        this.$axios.put(`${this.$plans}/${payload.id}`, payload).then(({data}) => {
-          this.successNotification(data, 'updated', 'country', 'plans', 'name')
-          this.initialize()
-        })
-      })
+        this.$axios
+          .put(`${this.$plans}/${payload.id}`, payload)
+          .then(({ data }) => {
+            this.successNotification(
+              data,
+              "updated",
+              "country",
+              "plans",
+              "name"
+            );
+            this.initialize();
+          });
+      });
     },
-    priceTitle(price) {
-      console.log(price, 'price')
-      let newPrice = this.showPrice(price.price)
-      let suffix = '';
+  },
+  filters: {
+    displayPrice(price) {
+      if (!price.price) price.price = 0;
+      let newPrice = `${price.price.toFixed(2)} €`;
 
-      if (price.is_recurring) suffix += 'every '
-      if (price.billing_period == 1) suffix += 'day'
-      if (price.billing_period == 2) suffix += 'week'
-      if (price.billing_period == 3) suffix += 'month'
-      if (price.billing_period == 4) suffix += '3 months'
-      if (price.billing_period == 5) suffix += '6 months'
-      if (price.billing_period == 6) suffix += 'year'
+      const suffixes = ["day", "week", "month", "3 months", "6 months", "year"];
+      const types = ["day", "week", "month"];
+      let suffix = "";
 
-      return `${newPrice} / ${suffix}`
-    }
+
+      if (price.is_recurring) suffix += "every ";
+
+      if (price.billing_period == 7) suffix += `${price.recycle_count} ${types[price.recycle_type - 1]}${price.recycle_count > 1 ? 's' : ''}`;
+      else if (price.billing_period) suffix += suffixes[price.billing_period - 1];
+
+      return `${newPrice} / ${suffix}`;
+    },
   },
 };
 </script>
