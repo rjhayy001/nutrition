92a4b90d5c06ca7582d7c94f488465a0fdcb4a60
Rjhayy <reitheljay25@gmail.com>
2022-06-07 14:08:03 +0800

    feat: tracking design

---
+++ components/clients/coaching/tracking/daily.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header class="text-capitalize">Les Daily</v-expansion-panel-header>
+    <v-expansion-panel-content>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
---
+++ components/clients/coaching/tracking/macro.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header class="text-capitalize">Les Macros</v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <div>
+        <statistics-component
+        :statistics="formattedStatistics"
+        :title="title"
+      />
+      </div>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+<script>
+import statisticsComponent from "@/components/common/statisticComponent.vue";
+export default {
+  components: {
+    statisticsComponent
+  },
+  data(){
+    return {
+      title: "",
+    }
+  },
+  computed: {
+    formattedStatistics() {
+      return [
+        {
+          title:'Protein',
+          value: 0,
+          type: 'number'
+        },
+        {
+          title:'Lipides',
+          value: 1,
+          type: 'number'
+        },
+        {
+          title:'Glucides',
+          value: 2,
+          type: 'number'
+        },
+        {
+          title:'Legumes',
+          value: 3,
+          type: 'number'
+        },
+      ]
+    }
+  },
+}
+</script>
